3c6edb4ff45ebc84e15119c5d7226e0689608452
omerkempner <48642602+omerkempner@users.noreply.github.com>
2021-12-29 18:47:56 +0200

    Add files via upload
    hackathon

---
+++ Client.py
@@ -0,0 +1,130 @@
+import struct
+import threading
+import time
+from select import select
+from socket import *
+
+import scapy.all as scapy
+import queue
+from getch import *
+
+
+game_time = 10
+magic_cookie = 0xabcddcba
+# client_ip = scapy.get_if_addr("eth1")
+client_ip = ''
+print(client_ip)
+
+
+def send_result(client_tcp_socket, time_out):
+    # get_data_thread = threading.Thread(target=get_result, args=(data, time_out))
+    # get_data_thread.start()
+    # print("in send result")
+    while time.time() < time_out:
+        data  = getch()
+        try:
+            print(data)
+            client_tcp_socket.send(data.encode('utf-8'))
+            break
+        except:  # ConnectionAbortedError | ConnectionResetError:
+            break
+
+# def get_result(data, time_out):
+#     while time.time() < time_out:
+#         data  = getch()
+
+
+
+# function for getting message from server after the game starts
+def get_from_server(client_tcp_socket):
+    while True:
+        try:
+            data = client_tcp_socket.recv(1024)
+            print(data.decode())
+            if not data:
+                break
+        except:
+            print("SERVER DISCONNECTED")
+            break
+
+
+
+def start():
+        client_udp_socket = socket(AF_INET, SOCK_DGRAM)
+        client_udp_socket.bind((client_ip, 13115))
+        client_udp_socket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+        client_udp_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        client_udp_socket.setsockopt(SOL_SOCKET, SO_REUSEPORT, 1)
+
+        print("insert client name")
+        client_name = input()
+
+        in_game = False
+        print("Client started, listening for offer requests...")
+
+        while not in_game:
+            print("recv msg from server")
+            try:
+                data, serverAddress = client_udp_socket.recvfrom(7)
+            except: 
+                continue 
+            print("after recv msg from server")
+            (sent_cookie, msg_type, msg_server_port) = struct.unpack('!IbH', data)
+            (ip, port) = serverAddress
+            print("server ip: " + str(ip) + " server port: " + str(port) + " port2: " + str(msg_server_port) )
+
+            if sent_cookie == magic_cookie and msg_type == 0x2:
+                print("Received offer from: " + ip + " , attempting to connect... ")
+
+                # accepting offer, sending team name
+                client_tcp_socket = socket(AF_INET, SOCK_STREAM)
+                try:
+                    print("try to connec\n")
+                    client_tcp_socket.connect((ip, msg_server_port))
+                    print("send name\n")
+                    client_tcp_socket.send(client_name.encode("utf-8"))
+                except:
+                    print("Server Disconnected")
+                    break
+
+                in_game = True
+                print("in game!\n")
+                client_udp_socket.close()
+
+                try:
+                    data = client_tcp_socket.recv(2048)
+                    print("print what the client recived")
+                    print(data.decode())
+                except Exception:
+                    print("server disconnected")
+                    break
+
+  # game starting
+                time_out = time.time() + game_time
+
+    
+                send_data_thread = threading.Thread(target=send_result, args=(client_tcp_socket, time_out,))
+                get_from_server_thread = threading.Thread(target=get_from_server, args=(client_tcp_socket,))
+                
+                send_data_thread.start()
+                get_from_server_thread.start()
+                
+                send_data_thread.join()
+                get_from_server_thread.join()
+                
+
+                #lahan neta 
+                # send_data_thread = threading.Thread(target=send_result, args=(data, client_tcp_socket, time_out,))
+                
+                # get_from_server_thread = threading.Thread(target=get_from_server, args=(client_tcp_socket,))
+                
+                # send_data_thread.start()
+                # get_from_server_thread.start()
+                # send_data_thread.join()
+                # get_from_server_thread.join()
+
+                client_tcp_socket.close()
+
+                print("Server disconnected, listening for offer requests...")
+
+start()
---
+++ Server.py
@@ -0,0 +1,297 @@
+import struct
+import random
+import time
+from socket import *
+# from _thread import *
+import threading
+
+import scapy.all as scapy
+
+l = threading.Lock()
+
+BrightBlack = u"\u001b[30;1m"
+BrightRed = u"\u001b[31;1m"
+BrightGreen_ANSI = u"\u001b[32;1m"
+BrightYellow = u"\u001b[33;1m"
+BrightBlue_ANSI = u"\u001b[34;1m"
+BrightMagenta = u"\u001b[35;1m"
+BrightCyan = u"\u001b[36;1m"
+BrightWhite = u"\u001b[37;1m"
+Black = u"\u001b[30m"
+BackgroundBrightWhite_ANSI = u"\u001b[47;1m"
+BackgroundGreen_ANSI = u"\u001b[42m"
+BackgroundBrightRed = u"\u001b[41;1m"
+Reset = u"\u001b[0m"
+Bold = u"\u001b[1m"
+
+accepting_time = 30
+game_time = 20
+reloading_time = 20
+
+global_timer = 10
+threaded_client_list = {}
+
+client1_name = ""
+client2_name = ""
+
+client1_conn = 0 
+client2_conn = 0 
+
+result = 0
+
+continue_game = True
+first_ans = -1 
+ans = -1 
+
+server_ip = scapy.get_if_addr("eth1")
+# server_ip = '127.0.0.1'
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+serverSocket.bind((server_ip, 0))
+serverSocket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)  # enable broadcasts
+serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+serverSocket.setsockopt(SOL_SOCKET, SO_REUSEPORT, 1)
+
+server_tcp_socket = socket(AF_INET, SOCK_STREAM)
+server_tcp_socket.bind((server_ip, 0))
+server_tcp_socket.listen(1)
+
+broadcast_address = '172.1.255.255'
+broadcast_port = 13115
+
+magic_cookie = 0xabcddcba
+message_type = 0x2
+server_tcp_port = server_tcp_socket.getsockname()[1]
+print("server tcp port " + str(server_tcp_port))
+
+def create_random_equation():
+    operator_list = ["+", "-"]
+    equation_numbers = random.randint(2, 3)
+    eq = str(random.randint(1, 20))
+    number= int(eq)
+    sum = number
+    for i in range(1, equation_numbers):
+        oper_index = random.randint(0, 1)
+        eq += operator_list[oper_index]
+        temp = str(random.randint(1, 10))
+        number= int(temp)
+        if (oper_index  == 0):
+            sum= sum +number
+        else:
+            sum= sum -number
+        eq += temp
+
+    meesage_q = "How much is "
+    meesage_q += eq
+    meesage_q += "?\n\n"
+    
+    result =sum
+
+    return meesage_q
+
+
+
+# function that manages the game with the client
+def thread_for_game(connection, client_name):
+    global continue_game, first_ans, ans
+    print("thread for game")
+    while time.time() < global_timer and continue_game:
+            try:
+                print("set connection")
+                connection.settimeout(max(global_timer - time.time(), 0))
+                data = connection.recv(2048)
+                if data:
+                    l.acquire()
+                    continue_game = False 
+                    first_ans = client_name 
+                    ans = data.decode('utf-8')
+                    print("ans: " + ans + " from user: " + first_ans)
+                    l.relese()       
+                    break
+                   #compare data to the correct ans
+                else: 
+
+                    print("in else")
+            except Exception:
+                print("in exception")
+                continue
+   
+    #TODO: manage game for each client here
+
+# function for getting the team name from the client
+def threaded_client(connection, client_num, timer_for_connections):
+    print("here only if tcp connection created with client " + str(client_num))
+    got_team_name = False
+    try:
+        connection.settimeout(max(timer_for_connections - time.time(), 0))
+        data = connection.recv(2048)
+        got_team_name = True
+    except Exception:
+        return
+
+    if got_team_name:
+        thread_team_name = data.decode('utf-8')
+        print("here if got name for client " + str(client_num) + " name " + thread_team_name)
+
+        l.acquire()
+        if client_num == 1:
+            global client1_conn, client1_name
+            client1_name = thread_team_name
+            print("assign client1_conn")
+            client1_conn = connection
+        else:
+            global client2_conn, client2_name
+            client2_name = thread_team_name
+            client2_conn = connection
+            print("assign client2_conn")
+        l.release()
+    else:
+        threaded_client_list.pop(client_num)
+        connection.close()
+
+def accept_connections():
+    #run until 2 clients connect
+    #wait 10 secondes for each connection
+    timer_for_connections = time.time() + accepting_time
+    client1 = False
+    client2 = False
+
+    while not client1 or not client2:
+        try:
+            server_tcp_socket.settimeout(max(timer_for_connections - time.time(), 0))
+            conn, address = server_tcp_socket.accept()
+        except Exception:
+            break
+
+        if not client1:
+            client_num = 1
+            client1 = True
+            print("client 1 joined")
+        elif not client2:
+            client_num = 2
+            client2 = True
+            print("client2 joined")
+        else:
+            break
+
+        #create thread for each client and start it 
+        t = threading.Thread(target=threaded_client,
+                                args=(conn, client_num, timer_for_connections,))
+        
+        threaded_client_list[client_num] = t
+        print("start threaded client")
+        t.start()
+
+
+def start_game():
+    #teams_dictionary = {}
+    client1 = {}
+    client2 = {}
+    
+    print(BrightBlack + BackgroundBrightWhite_ANSI + "  Server started, listening on IP address:" + server_ip + Reset)
+
+    invite_message = struct.pack('!IbH', magic_cookie, message_type, server_tcp_port) #IbH - big endian\little endian.. format
+
+    #thread for connections 
+    print("before accecpt connections thread\n")
+    t = threading.Thread(target=accept_connections, args=())
+    t.start()
+
+    for i in range(10):
+        serverSocket.sendto(invite_message, (broadcast_address, broadcast_port))
+        time.sleep(1)
+
+    t.join()
+
+    if len(threaded_client_list) == 0 or client1_conn == 0 or client2_conn == 0:
+        print(BrightBlue_ANSI + "No one has connected, or No one sent team name :(" + Reset + "\n")
+        return
+
+    # welcome message
+    welcome_message = BrightBlack + BackgroundBrightWhite_ANSI + Bold
+    welcome_message += "$Welcome to Quick Maths$" + Reset + "\n"
+    welcome_message += BrightGreen_ANSI + BackgroundGreen_ANSI + "Player 1:  " 
+    welcome_message += client1_name + Reset 
+    welcome_message += "\n" + BackgroundBrightRed
+    welcome_message += BrightWhite + "Player 2:  " 
+    welcome_message += client2_name
+    welcome_message += Reset
+    welcome_message += "\n"
+    welcome_message += "==\n"
+    welcome_message += "Please answer the following question as fast as you can:\n\n"
+    #print(welcome_message)
+    # end welcome message
+
+    message_q = create_random_equation()
+
+    welcome_message += message_q
+
+    client1_conn.send(welcome_message.encode('utf-8'))
+    client2_conn.send(welcome_message.encode('utf-8'))
+
+
+    print(BrightRed + " ************************************" + Reset)
+
+    # game starts
+    l.acquire()
+    global global_timer
+    global_timer = time.time() + game_time
+    l.release()
+
+    client1_threadGame = threading.Thread(target=thread_for_game, args=(client1_conn, client1_name))
+    client2_threadGame = threading.Thread(target=thread_for_game, args=(client2_conn, client2_name))
+    
+    client1_threadGame.start()
+    client2_threadGame.start()
+    
+    client1_threadGame.join()
+    client2_threadGame.join()
+
+
+    end_message =  """
+           _____       ___       ___  ___   _____  
+          /  ___|     /   |     /   |/   | |  ___| 
+          | |        / /| |    / /|   /| | | |__   
+          | |  _    / ___ |   / / |__/ | | |  __|  
+          | |_| |  / /  | |  / /       | | | |___  
+          \_____/ /_/   |_| /_/        |_| |_____|
+           _____   _     _   _____   _____   
+          /  _  \ | |   / / |  ___| |  _  \  
+          | | | | | |  / /  | |__   | |_| |  
+          | | | | | | / /   |  __|  |  _  /  
+          | |_| | | |/ /    | |___  | | \ \  
+          \_____/ |___/     |_____| |_|  \_\
+          """
+
+    end_message += "The correct answer was " + result + "!\n"
+    
+
+
+    if first_ans == -1: 
+       end_message += "DRAW :( \n" 
+    elif ans == result: 
+        end_message += "Congratulations to the winner: " + first_ans 
+    else: 
+        if first_ans == client1_name: 
+            end_message += "Congratulations to the winner: " + client2_name 
+        else: 
+            end_message += "Congratulations to the winner: " + client1_name
+
+
+    try:
+        client1_conn.send(end_message.encode('utf-8'))
+        client2_conn.send(end_message.encode('utf-8'))
+    except Exception:
+        print("exception")
+ 
+    # time.sleep(game_time)
+
+
+    client1_conn.close()
+    client2_conn.close()
+
+    print("Server Reloading")
+    time.sleep(reloading_time)
+
+
+while True:
+    start_game()
